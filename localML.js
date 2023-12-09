3f6e01b5585c47770303797d91ac7adbeaf4778c
Nick Kantack <kantackn@amazon.com>
2023-12-09 15:55:10 -0500

    Added hand pose code

--- localML.js
+++ localML.js
@@ -1,11 +1,16 @@
 import { AmazonConnectApp } from "@amazon-connect/app";
 
+// Configure logic around the hand pose
+handPoseUpdate = function(hands) {
+    handIndicator.innerHTML = hands && hands.length ? hands[0].score : 0;
+}
+
 const { connectApp } = AmazonConnectApp.init({
-onCreate: (event) => {
-    const { appInstanceId } = event.context;
-    console.log('App initialized: ', appInstanceId);
-},
-onDestroy: (event) => {
-    console.log('App being destroyed');
-},
+    onCreate: (event) => {
+        const { appInstanceId } = event.context;
+        console.log('App initialized: ', appInstanceId);
+    },
+    onDestroy: (event) => {
+        console.log('App being destroyed');
+    },
 }); 
